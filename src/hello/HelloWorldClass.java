1762d894cd88709b912fc3a8f382f8a837896170
Rabin <rabin.shrestha7@gmail.com>
2023-04-21 06:59:13 -0700

    commit2

--- src/hello/HelloWorldClass.java
+++ src/hello/HelloWorldClass.java
@@ -5,6 +5,7 @@ public class HelloWorldClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiiiiiiii");
+		System.out.println("heloooooooo");
 
 	}
 
