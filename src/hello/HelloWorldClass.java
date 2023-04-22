54014e2fb58deccb1f09b9b8aa977b125c8bf711
Rabin <rabin.shrestha7@gmail.com>
2023-04-22 11:56:07 -0700

    what

--- src/hello/HelloWorldClass.java
+++ src/hello/HelloWorldClass.java
@@ -7,12 +7,8 @@ public class HelloWorldClass {
 		System.out.println("hiiiiiiii");
 		System.out.println("heloooooooo");
 		System.out.println("heyyyyyyyyy");
-<<<<<<< HEAD
 		System.out.println("hhhhhhhhhh");
 		System.out.println("whats upppppp");
-=======
-		System.out.println("i am at branch");
->>>>>>> 4e2c928 (just add)
 
 	}
 
